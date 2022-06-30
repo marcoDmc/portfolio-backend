bfa694960de5d73716e0d8d1129663ce1ae148e9
mdev <marcodamasceno07@gmail.com>
2022-06-30 03:53:46 -0300

    :fire: remove code

--- controllers/handleSendEmail.js
+++ controllers/handleSendEmail.js
@@ -3,20 +3,23 @@ const Transport = require('../models/nodemailer.config.js')
 module.exports = {
   sendEmail(req, res) {
     const { name, email, message } = req.body
-    if (!name || !email || !message)
-      return res.status(401).json({ error: 'something is missing here' })
-    Transport.sendMail({
-      from: name,
-      to: email,
-      subject: 'nova mensagem',
-      html: `<h1>uma mensagem de ${name}</h1> 
+
+    try {
+      if (!name || !email || !message) {
+        res.status(401).json({ error: 'something is missing here' })
+      } else {
+        Transport.sendMail({
+          from: name,
+          to: email,
+          subject: 'nova mensagem',
+          html: `<h1>uma mensagem de ${name}</h1> 
                 <p>Message: <br/>${message}</p>`,
-      text: `uma mensagem de ${name} 
+          text: `uma mensagem de ${name} 
                 Message: ${message}`,
-    })
-      .then(() => res.status(200).json({ msg: 'success' }))
-      .catch((e) =>
-        res.status(400).json({ msg: `error , email could not be sent ${e}` }),
-      )
+        }).then(() => res.status(200).json({ msg: 'success' }))
+      }
+    } catch (error) {
+      res.status(400).json({ msg: `error , email could not be sent ${error}` })
+    }
   },
 }
