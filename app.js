b6640b5d0d79ae06f63f7bd2e60e8735565733a9
mdev <marcodamasceno07@gmail.com>
2022-06-30 08:35:11 -0300

    :bug: configured heroku default port

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const  port = 80;
 const app = express();
 const router = require("./routes/router.js");
 
@@ -7,4 +6,4 @@ const router = require("./routes/router.js");
 app.use(express.json());
 app.use(router);
 
-app.listen(port || 5000 ,() => console.log(`server running on port ${port}`) )
+app.listen(process.env.PORT || 5000 ,() => console.log(`server running on port ${port}`) )
