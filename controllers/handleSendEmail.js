3e132f35745df4773015546d6747dc61a3afcd12
mdev <marcodamasceno07@gmail.com>
2022-06-30 03:32:51 -0300

    :sparkles: validating empty fields

--- controllers/handleSendEmail.js
+++ controllers/handleSendEmail.js
@@ -1,21 +1,22 @@
-const Transport = require("../models/nodemailer.config.js");
-
+const Transport = require('../models/nodemailer.config.js')
 
 module.exports = {
-     sendEmail(req,res){
-        const {name , email, message} = req.body;
-      
-       Transport.sendMail({
-                from:name,
-                to:email,
-                subject:"nova mensagem",
-                html: `<h1>uma mensagem de ${name}</h1> 
+  sendEmail(req, res) {
+    const { name, email, message } = req.body
+    if (!name || !email || !message)
+      return res.status(401).json({ error: 'something is missing here' })
+    Transport.sendMail({
+      from: name,
+      to: email,
+      subject: 'nova mensagem',
+      html: `<h1>uma mensagem de ${name}</h1> 
                 <p>Message: <br/>${message}</p>`,
-                text:`uma mensagem de ${name} 
-                Message: ${message}`
-       })
-            .then(() => res.status(200).json({ msg: "success" }))
-            .catch((e) =>  res.status(400).json({msg:`error , email could not be sent ${e}`}))
-    }
+      text: `uma mensagem de ${name} 
+                Message: ${message}`,
+    })
+      .then(() => res.status(200).json({ msg: 'success' }))
+      .catch((e) =>
+        res.status(400).json({ msg: `error , email could not be sent ${e}` }),
+      )
+  },
 }
-
