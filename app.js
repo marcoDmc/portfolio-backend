efb2ffa54d85952e1e293d3c118b5f4887b3b2e6
mdev <marcodamasceno07@gmail.com>
2022-07-01 03:20:01 -0300

    .

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const router = require("./routes/router.js");
 
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
-app.listen(process.env.PORT || 3000 ,() => console.log(`server running on port ${port}`) )
+app.listen(process.env.PORT || 3000 ,() => console.log(`server running`) )
