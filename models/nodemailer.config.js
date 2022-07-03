aa7e7ff81ecde2c1f85bc3cdbecded1842172cb0
marco <marcodamasceno07@gmail.com>
2022-07-03 18:59:24 -0300

    format code

--- models/nodemailer.config.js
+++ models/nodemailer.config.js
@@ -1,14 +1,13 @@
-const nodeMailer = require("nodemailer");
-require("dotenv").config();
+const nodeMailer = require('nodemailer')
+require('dotenv').config()
 
 const transport = nodeMailer.createTransport({
-    host: process.env.HOST,
-    port: process.env.PORT_EMAIL,
-    secure:true,
-    auth:{
-        user: process.env.MY_EMAIL,
-        pass: process.env.MY_PASSWORD,
-    }
-
+  host: process.env.HOST,
+  port: process.env.PORT_EMAIL,
+  secure: true,
+  auth: {
+    user: process.env.MY_EMAIL,
+    pass: process.env.MY_PASSWORD,
+  },
 })
-module.exports = transport;
+module.exports = transport
