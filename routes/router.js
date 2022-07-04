6d07ce5d34ab251733dcb54ca0cb93c9b3fbebb1
marco <marcodamasceno07@gmail.com>
2022-07-04 01:26:02 -0300

    fix: trying to troubleshoot puppeteer , heroku can't load file contents

--- routes/router.js
+++ routes/router.js
@@ -2,13 +2,8 @@ const Express = require('express')
 const router = Express.Router()
 const func = require('../controllers/handleSendEmail.js')
 const pdf = require('../controllers/handleGeneratePdf')
-const timeout = require('connect-timeout')
 
-
- function haltOnTimedout (req, res, next) {
-    if (!req.timeout) next()
-  }
-router.get('/pdf', timeout('30s'),haltOnTimedout, pdf.handleSendPDF)
+router.get('/pdf', pdf.handleSendPDF)
 router.get('/curriculum', pdf.handleRenderEjs)
 router.post('/', func.sendEmail)
 
